cb448ef53d8a8994511b4d7ee54560d5741b12e7
SALMAN PATNI <salmanchohan566@gmail.com>
2024-03-19 14:16:52 -0700

    tweak the text

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
+#! /usr/bin/env node
+
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstnumber" },
-  { message: "Enter second number", type: "number", name: "secondnumber" },
-  {message: "Select one of the operators to perform operation",
+  { message: "Salman please Enter your first number", type: "number", name: "firstnumber" },
+  { message: "Salman please Enter your second number", type: "number", name: "secondnumber" },
+  {message: "Select one of the operator to perform operation",
   type: "list",
   name: "operator",
   choices: ["addition", "subtraction","multiplication","division"]
